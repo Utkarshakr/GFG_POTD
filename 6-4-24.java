88f7f1813a26f6ab11f1a9dff5dbed2d1f8f9dd4
Utkarshakr <146371935+Utkarshakr@users.noreply.github.com>
2024-04-11 04:55:07 +0000

    Count ways to N'th Stair - 6-4-24

---
+++ 6-4-24.java
@@ -0,0 +1,11 @@
+
+
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        // your code here
+        long ans=0;
+       return ans=1+ n/2;
+    }
+}
